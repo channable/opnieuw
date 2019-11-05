8fbecab4d06e9649748d05a09bc22c9b96ca0fe8
Ruud van Asseldonk <ruud@channable.com>
2019-11-05 11:01:22 +0100

    Fix reading description in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from retry import __version__
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -10,7 +10,7 @@ setuptools.setup(
     author="Channable",
     author_email="ruud@channable.com",
     description="Retries for humans",
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/channable/workingtitle-retry",
     packages=setuptools.find_packages(),
