9a3c1ee7879be371d671950f17e319cbbf6b5e97
Jochem Bijlard <jochem.bijlard@channable.com>
2020-01-28 18:08:27 +0100

    Minor details

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+# Opnieuw: Retries for humans
+# Copyright 2019 Channable
+#
+# Licensed under the 3-clause BSD license, see the LICENSE file in the repository root.
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+__version__ = "0.0.1"
+
 setuptools.setup(
     name="opnieuw",
-    version="0.0.1",
+    version=__version__,
     author="Channable",
     author_email="ruud@channable.com",
     description="Retries for humans",
