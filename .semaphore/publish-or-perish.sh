b10d261f1fa092a90dcad6188efc33a4ba0155c9
Jochem Bijlard <jochem.bijlard@channable.com>
2020-01-31 17:00:30 +0100

    Some best practices for publish script.

--- .semaphore/publish-or-perish.sh
+++ .semaphore/publish-or-perish.sh
@@ -4,15 +4,14 @@
 # it reaches the end of the file, without Semaphore thinking it
 # has failed.
 
-set -v
+set -efuo pipefail
 
 git fetch --tags
 TAG=v$(python3.7 setup.py --version)
 # If the tag already exist this command will fail and the job will exit
 # without raising an error. Otherwise, we will build the project,
 # publish to PyPi, and push the tag to github.
-git tag $TAG || exit 0
+git tag "$TAG" || exit 0
 sudo pip install twine
-python3.7 setup.py sdist bdist_wheel
 twine upload --skip-existing dist/*
-git push origin $TAG
+git push origin "$TAG"
