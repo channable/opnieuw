312b5cdc8010161dfd3bde163b143e6576f6b5d7
Bernhard Janetzki <boerni@gmail.com>
2020-02-13 16:47:22 +0100

    Add support for max_calls_total=1

--- tests/test_exponential_multiplier.py
+++ tests/test_exponential_multiplier.py
@@ -10,6 +10,15 @@ from opnieuw.retries import calculate_exponential_multiplier
 
 class ExponentialMultiplierTests(unittest.TestCase):
     def test_calculate_exponential_multiplier(self):
+        # test for max_calls_total=1 and 2
+        expected_multiplier = 120.0
+        multiplier = calculate_exponential_multiplier(1, 120)
+        self.assertEqual(multiplier, expected_multiplier)
+
+        multiplier = calculate_exponential_multiplier(2, 120)
+        self.assertEqual(multiplier, expected_multiplier)
+
+        # test max_calls_total > 2
         expected_multiplier = 8.0
         multiplier = calculate_exponential_multiplier(5, 120)
 
