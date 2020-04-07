b89f608efff1cf864a251e81a1fec195e660899f
Jochem Bijlard <jochem.bijlard@channable.com>
2020-04-07 13:37:10 +0200

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "0.0.3"
+__version__ = "1.0.0"
 
 setuptools.setup(
     name="opnieuw",
